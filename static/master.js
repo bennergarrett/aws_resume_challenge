932ac105ad386bb8b1079ac1d82738d66cf97209
bennergarrett <bennergarrett@gmail.com>
2020-09-02 11:58:43 -0500

    points to backend now

--- static/master.js
+++ static/master.js
@@ -3,16 +3,18 @@
 function counter() {
   let obj;
   
-  fetch('https://penx8v1wwj.execute-api.us-east-1.amazonaws.com/Production')
+  fetch('https://xgjr28nes6.execute-api.us-east-1.amazonaws.com/Prod/%7Bproxy+%7D')
     .then(res => res.json())
     .then(data => obj = data)
     .then(() => {
-      console.log(obj)
+      console.log(obj.message)
       let content = document.getElementById('counter');
       let content2 = document.getElementById('counter2');
-      let newString = document.createTextNode(obj)
+      let newString = document.createTextNode(obj.message)
+      let newString2 = document.createTextNode(obj.message)
+
       content.appendChild(newString); //loads onto page
-      content2.appendChild(newString); //loads onto page
+      content2.appendChild(newString2); //loads onto page
 
 
     })
